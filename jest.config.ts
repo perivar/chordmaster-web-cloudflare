f959083ddd6b1d125ff47f901e978c7ccedea49f
Per Ivar Nerseth <perivar@nerseth.com>
2024-11-20 22:59:05 +0100

    changed jest.config to import type from jest and upgrade packages

--- jest.config.ts
+++ jest.config.ts
@@ -1,18 +1,18 @@
-import { Config } from "@jest/types";
+import type { Config } from "jest";
 
 // By default, all files inside `node_modules` are not transformed. But some 3rd party
 // modules are published as untranspiled, Jest will not understand the code in these modules.
 // To overcome this, exclude these modules in the ignore pattern.
 const untranspiledModulePatterns = ["chord-symbol"];
 
-const config: Config.InitialOptions = {
+const config: Config = {
   preset: "ts-jest",
   testEnvironment: "node",
   roots: ["app"],
   transform: {
     "\\.[jt]sx?$": ["ts-jest", { tsconfig: "tsconfig.json" }],
   },
-  testRegex: "/__tests__/.*.test.ts$",
+  testRegex: "/__tests__/.*.(spec|test).ts$",
   transformIgnorePatterns: [
     `node_modules/(?!${untranspiledModulePatterns.join("|")})`,
   ],
