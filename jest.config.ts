bf600bc191d441da467513efaf6dcb38a6cb50a8
Per Ivar Nerseth <perivar@nerseth.com>
2024-12-23 18:02:54 +0100

    make jest typesafe

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,11 @@
-import type { Config } from "jest";
+import { Config } from "@jest/types"
 
 // By default, all files inside `node_modules` are not transformed. But some 3rd party
 // modules are published as untranspiled, Jest will not understand the code in these modules.
 // To overcome this, exclude these modules in the ignore pattern.
 const untranspiledModulePatterns = ["chord-symbol"];
 
-const config: Config = {
+const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "node",
   roots: ["app"],
